d7d234ceb245b636734250df725929710aa64ec5
Cacharel02 <cachareljunior@gmail.com>
2026-06-09 13:45:09 +0200

    initialization

---
+++ src/main/java/com/perso/fileZipperTool/FileZipperToolApplication.java
@@ -0,0 +1,13 @@
+package com.perso.fileZipperTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileZipperToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileZipperToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perso/fileZipperTool/FileZipperToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perso.fileZipperTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileZipperToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
