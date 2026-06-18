34845ad810b33b4c89c185b398394e39b596f83d
cacharel02 <cachareljunior@gmail.com>
2026-06-18 21:36:26 +0200

    fix cors

--- src/main/java/com/perso/fileZipperTool/controller/FileZipperController.java
+++ src/main/java/com/perso/fileZipperTool/controller/FileZipperController.java
@@ -5,16 +5,14 @@ import com.perso.fileZipperTool.utils.ZipUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173", exposedHeaders = "Content-Disposition")
 @RestController
 @RequestMapping("/filezippertool")
 public class FileZipperController {
